d832f6de870c930244faa753ef02d9e56c619ec5
Ex-iT <vic.toothpick@gmail.com>
2021-03-08 01:13:26 +0100

    Preparing for multi-monitor support

--- save-o-clock.py
+++ save-o-clock.py
@@ -1,47 +1,61 @@
 from tkinter import *
 from tkinter.font import Font
 from datetime import datetime
-
+from screeninfo import get_monitors
 
 class Save_O_Clock:
-    def __init__(self):
+    def __init__(self, monitor):
         self.root = Tk()
         self.root.title('Save-O-Clock')
         self.root.configure(background='black')
-        # self.root.geometry('1024x768')
+        # self.root.geometry(f'{monitor.width}x{monitor.height}+{monitor.x}+{monitor.y}')
         self.root.attributes('-fullscreen', True)
         self.root.bind_all('<Key>', self.close)
         self.root.bind_all('<Motion>', self.close)
 
         self.root.update_idletasks()
-        self.screen_width = self.root.winfo_screenwidth()
-        self.screen_height = self.root.winfo_screenheight()
 
-        self.frame = Frame(self.root, cursor='none')
+        self.createFrame(self, monitor)
+
+
+    def createFrame(self, event, monitor):
+        self.frame = Frame(
+            self.root,
+            cursor='none',
+            width=monitor.width,
+            height=monitor.height
+        )
         self.frame.pack()
 
-        self.clock = Label(
+        self.createClock(self, monitor)
+
+
+    def createClock(self, event, monitor):
+        self.label = Label(
             self.frame,
             text='',
             fg='white',
             bg='black',
-            width=self.screen_width,
-            height=self.screen_height,
+            width=monitor.width,
+            height=monitor.height,
             cursor='none'
         )
-        self.clock.configure(font=Font(family='JetBrains Mono', size=200))
-        self.clock.pack()
+        self.label.configure(font=Font(family='JetBrains Mono', size=200))
+        self.label.pack()
 
-        self.updateClock()
+        self.updateLabel(self)
 
-    def updateClock(self, event=None):
-        self.clock.configure(text=datetime.now().strftime('%H:%M'))
-        self.root.after(1000, self.updateClock)
+
+    def updateLabel(self, event=None):
+        self.label.configure(text=datetime.now().strftime('%H:%M'))
+        self.root.after(1000, self.updateLabel)
+
 
     def close(self, event=None):
         self.root.destroy()
 
 
 if __name__ == '__main__':
-    w = Save_O_Clock()
-    w.root.mainloop()
+    for monitor in get_monitors():
+        w = Save_O_Clock(monitor)
+        w.root.mainloop()
