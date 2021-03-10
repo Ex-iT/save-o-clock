7029a840645fe878dde25ac38583e9adbdd8274c
Ex-iT <vic.toothpick@gmail.com>
2021-03-10 17:13:58 +0100

    Added multi monitor support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Save-O-Clock
 
-A simple screensaver in Python with the [TKinter](https://wiki.python.org/moin/TkInter) package.
+A simple screensaver in Python with the [TKinter](https://wiki.python.org/moin/TkInter) package, supports multiple monitors.
 Displays a 24-hour clock (using [JetBrains Mono](https://www.jetbrains.com/lp/mono/) font) in the center of the screen.
 Closes on mouse movement or any keyboard input.
 
--- save-o-clock.py
+++ save-o-clock.py
@@ -3,59 +3,68 @@ from tkinter.font import Font
 from datetime import datetime
 from screeninfo import get_monitors
 
+time_format = '%H:%M:%S'
+
 class Save_O_Clock:
-    def __init__(self, monitor):
+    def __init__(self):
         self.root = Tk()
         self.root.title('Save-O-Clock')
-        self.root.configure(background='black')
-        # self.root.geometry(f'{monitor.width}x{monitor.height}+{monitor.x}+{monitor.y}')
-        self.root.attributes('-fullscreen', True)
+        self.root.configure(background='black', cursor='none')
+        self.root.geometry('0x0')
         self.root.bind_all('<Key>', self.close)
         self.root.bind_all('<Motion>', self.close)
-
         self.root.update_idletasks()
 
-        self.createFrame(self, monitor)
+        for monitor in get_monitors():
+            Create_Window(self.root, monitor)
+
+    def close(self, event=None):
+        self.root.destroy()
 
 
-    def createFrame(self, event, monitor):
+class Create_Window:
+    def __init__(self, root, monitor):
+        self.monitor = monitor
+        self.root = Toplevel(root)
+        self.root.configure(background='black')
+        self.root.geometry(f'{self.monitor.width}x{self.monitor.height}+{self.monitor.x}+{self.monitor.y}')
+        self.root.update_idletasks()
+        self.root.attributes('-topmost', True)
+        self.root.attributes('-fullscreen', True)
+
+        self.createFrame(self)
+
+    def createFrame(self, event=None):
         self.frame = Frame(
             self.root,
             cursor='none',
-            width=monitor.width,
-            height=monitor.height
+            width=self.monitor.width,
+            height=self.monitor.height
         )
         self.frame.pack()
 
-        self.createClock(self, monitor)
+        self.createClock(self)
 
-
-    def createClock(self, event, monitor):
+    def createClock(self, event=None):
         self.label = Label(
             self.frame,
+            cursor='none',
             text='',
             fg='white',
             bg='black',
-            width=monitor.width,
-            height=monitor.height,
-            cursor='none'
+            width=self.monitor.width,
+            height=self.monitor.height
         )
         self.label.configure(font=Font(family='JetBrains Mono', size=200))
         self.label.pack()
 
         self.updateLabel(self)
 
-
     def updateLabel(self, event=None):
-        self.label.configure(text=datetime.now().strftime('%H:%M'))
-        self.root.after(1000, self.updateLabel)
-
-
-    def close(self, event=None):
-        self.root.destroy()
+        self.label.configure(text=datetime.now().strftime(time_format))
+        self.root.after(250, self.updateLabel)
 
 
 if __name__ == '__main__':
-    for monitor in get_monitors():
-        w = Save_O_Clock(monitor)
-        w.root.mainloop()
+    w = Save_O_Clock()
+    w.root.mainloop()
